66eab5a9b86606c3beebd932e35df05a85e3d938
unknown <ajoy9532@gmail.com>
2023-06-04 19:26:01 +0600

    added status function

--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,16 +1,41 @@
 import React from "react";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import useTask from "../Hooks/useTask";
+import Swal from "sweetalert2";
 
 const TaskList = ({ singleTask, index }) => {
-  const { _id, task, description } = singleTask || {};
+  const [, refetch] = useTask();
+  const { _id, task, description, status } = singleTask || {};
+
+  const handleComplete = (id) => {
+    fetch(`http://localhost:5000/task/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          Swal.fire("Good job!", "Task Strikethrough Successfully!", "success");
+        }
+      });
+  };
 
   return (
     <tr>
       <td>{index + 1}.</td>
-      <td>{task}</td>
-      <td>{description}</td>
+      <td className={status === "complete" && "line-through"}>{task}</td>
+      <td className={status === "complete" && "line-through"}>{description}</td>
       <td>
-        <button className="btn btn-success btn-xs">Complete</button>
+        <button
+          disabled={status === "complete"}
+          onClick={() => handleComplete(_id)}
+          className="btn btn-success btn-xs">
+          {status === "complete" ? (
+            <span>completed</span>
+          ) : (
+            <span>pending</span>
+          )}
+        </button>
       </td>
       <td>
         <button className="btn btn-info btn-sm hover:text-white">
