95007fb909b5ff81a5df84590f1fb03a43a0bf3c
unknown <ajoy9532@gmail.com>
2023-06-04 16:23:07 +0600

    added taskList

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import InputForm from "./InputForm";
+import TaskList from "./TaskList";
 
 const Home = () => {
   return (
     <div className="mx-auto">
       <div className="flex lg:flex-row flex-col-reverse">
-        <div className="lg:w-1/2">
-          <h1>this is list section</h1>
+        <div className="lg:w-3/5">
+          <div className="overflow-x-auto mt-8 lg:w-4/6 w-screen mx-auto">
+            <table className="table table-compact w-full">
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Status</th>
+                  <th>Update</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tbody></tbody>
+            </table>
+          </div>
         </div>
-        <div className="lg:w-1/2">
+        <div className="lg:w-2/5">
           <InputForm></InputForm>
         </div>
       </div>
